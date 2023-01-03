6acccb89727157959969ba1ee71ebbf542449169
zya <395549131@qq.com>
2023-01-03 22:22:18 +0800

    blog4 add comment3

--- blog4/app/controllers/comments_controller.rb
+++ blog4/app/controllers/comments_controller.rb
@@ -21,14 +21,16 @@ class CommentsController < ApplicationController
 
   # POST /comments or /comments.json
   def create
+    @blog = Blog.find(params[:blog_id])
     @comment = Comment.new(comment_params)
+    @comment.blog = @blog
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to comment_url(@comment), notice: "Comment was successfully created." }
+        format.html { redirect_to @blog, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render "blogs/show" }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
