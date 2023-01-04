a1f43e8d8fb4abebc987bbe9e58687f1efe0118a
20373384 <395549131@qq.com>
2023-01-04 08:23:58 +0000

    project

--- blog4/db/schema.rb
+++ blog4/db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_03_140202) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_03_163131) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -23,8 +25,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_03_140202) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["blog_id"], name: "index_comments_on_blog_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "blogs", "users"
   add_foreign_key "comments", "blogs"
+  add_foreign_key "comments", "users"
 end
