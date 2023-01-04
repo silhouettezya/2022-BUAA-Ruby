0ffc13f21840106b4e95252d6fca8c48b222908f
zya <395549131@qq.com>
2023-01-04 22:09:37 +0800

    project

--- project/config/routes.rb
+++ project/config/routes.rb
@@ -1,6 +1,26 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :products
+  resources :designs
+  get 'welcome/index'
+  resources :favorites
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :cart_items
+  post 'cart_items/increase/:id' => 'cart_items#increase_cart_item_count'
+  post 'cart_items/decrease/:id' => 'cart_items#decrease_cart_item_count'
+  resources :transaction_items
+  resources :transaction_orders
+  post 'transaction_orders/transfer/pay/:id' => 'transaction_orders#transfer_order_status_pay'
+  post 'transaction_orders/transfer/deliver/:id' => 'transaction_orders#transfer_order_status_deliver'
+  post 'transaction_orders/transfer/get/:id' => 'transaction_orders#transfer_order_status_get'
+  resources :colors
+  resources :product_types
+  resources :sizes
+  devise_for :users
+  resources :users
+
+
+
+
+  root 'welcome#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
