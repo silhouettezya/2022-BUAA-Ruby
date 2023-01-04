f4baed38cb3e7cde650af32bf6212d1e47df33c7
zya <395549131@qq.com>
2023-01-04 21:31:39 +0800

    project test

--- project/config/application.rb
+++ project/config/application.rb
@@ -18,5 +18,10 @@ module Project
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators do |generator|
+      generator.assets false
+      generator.test_framework false
+    end
   end
 end
