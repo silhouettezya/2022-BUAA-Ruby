8ce463a2ee60532eece264842c5d367c151a2ec2
zya <395549131@qq.com>
2023-01-06 03:23:29 +0800

    blog4

--- blog4/config/routes.rb
+++ blog4/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :blogs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root 'blogs#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
