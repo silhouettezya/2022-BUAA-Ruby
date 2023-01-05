0278a7c8dbf7b1c0a993c6b0acd32fd7aa71a76c
zya <395549131@qq.com>
2023-01-05 12:29:05 +0800

    user regin

--- project/app/models/user.rb
+++ project/app/models/user.rb
@@ -18,6 +18,10 @@ class User < ApplicationRecord
   validates_length_of :password, message: "密码最短为6位", minimum: 6,
                       if: :need_validate_password
 
+  def username
+    self.email.split('@').first
+  end
+
   private
   def need_validate_password
     self.new_record? ||
