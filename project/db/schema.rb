cf055698c1560113ee6e74aa659a50bff319bbb5
20373384 <395549131@qq.com>
2023-01-05 14:32:02 +0000

    web

--- project/db/schema.rb
+++ project/db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_05_122041) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_05_130653) do
+  create_table "addresses", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "address_type"
+    t.string "contact_name"
+    t.string "cellphone"
+    t.string "address"
+    t.string "zipcode"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "address_type"], name: "index_addresses_on_user_id_and_address_type"
+  end
+
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.integer "weight", default: 0
@@ -66,6 +78,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_05_122041) do
     t.datetime "reset_password_email_sent_at"
     t.integer "access_count_to_reset_password_page", default: 0
     t.string "uuid"
+    t.integer "default_address_id"
     t.index ["activation_token"], name: "index_users_on_activation_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
