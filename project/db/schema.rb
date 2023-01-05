c242ae63b8bf3e87e223f48d1ae42504344e3765
20373384 <395549131@qq.com>
2023-01-05 14:33:56 +0000

    web

--- project/db/schema.rb
+++ project/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_05_130653) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_05_143134) do
   create_table "addresses", force: :cascade do |t|
     t.integer "user_id"
     t.string "address_type"
@@ -32,6 +32,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_05_130653) do
     t.string "ancestry"
     t.index ["ancestry"], name: "index_categories_on_ancestry"
     t.index ["title"], name: "index_categories_on_title"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.integer "address_id"
+    t.string "order_no"
+    t.integer "amount"
+    t.decimal "total_money", precision: 10, scale: 2
+    t.datetime "payment_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_no"], name: "index_orders_on_order_no", unique: true
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
