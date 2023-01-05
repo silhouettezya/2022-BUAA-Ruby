c8337346017a3fd33eaae6f3be4563c52287040f
zya <395549131@qq.com>
2023-01-05 20:22:31 +0800

    products show

--- project/db/migrate/20230105122041_add_user_uuid_column.rb
+++ project/db/migrate/20230105122041_add_user_uuid_column.rb
@@ -1,4 +1,12 @@
 class AddUserUuidColumn < ActiveRecord::Migration[7.0]
   def change
+    add_column :users, :uuid, :string
+
+    add_index :users, [:uuid], unique: true
+
+    User.find_each do |user|
+      user.uuid = RandomCode.generate_utoken
+      user.save
+    end
   end
 end
