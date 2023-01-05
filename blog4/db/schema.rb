6d78f4a04038c76cfdc55a69068f35291e300ef8
20373384 <395549131@qq.com>
2023-01-05 19:24:42 +0000

    web

--- blog4/db/schema.rb
+++ blog4/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_05_190452) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_05_192414) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -18,4 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_05_190452) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.integer "blog_id", null: false
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blog_id"], name: "index_comments_on_blog_id"
+  end
+
+  add_foreign_key "comments", "blogs"
 end
