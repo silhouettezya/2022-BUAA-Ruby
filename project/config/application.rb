851c3d0cd6acb720b76b8b82b87a0e1a7cde25e4
zya <395549131@qq.com>
2023-01-05 12:47:20 +0800

    user product

--- project/config/application.rb
+++ project/config/application.rb
@@ -18,6 +18,9 @@ module Project
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.autoload_paths += %W[#{Rails.root}/lib]
+
     config.generators do |generator|
       generator.assets false
       generator.test_framework false
