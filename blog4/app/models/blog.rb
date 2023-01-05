b9ef5200791c40f42ae9acf4a303d5b605a78501
zya <395549131@qq.com>
2023-01-06 03:03:15 +0800

    blog4

--- blog4/app/models/blog.rb
+++
@@ -1,5 +0,0 @@
-class Blog < ActiveRecord::Base
-  validates :title, :content,  presence: true
-  has_many :comments, dependent: :destroy
-  belongs_to :user
-end
--- blog4/app/models/comment.rb
+++
@@ -1,4 +0,0 @@
-class Comment < ApplicationRecord
-  belongs_to :blog
-  belongs_to :user
-end
