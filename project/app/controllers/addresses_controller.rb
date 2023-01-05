e23d8fd7ed71f09a256b7530b776153a44e83844
zya <395549131@qq.com>
2023-01-05 22:27:21 +0800

    fix address3

--- project/app/controllers/addresses_controller.rb
+++ project/app/controllers/addresses_controller.rb
@@ -38,12 +38,12 @@ class AddressesController < ApplicationController
       @addresses = current_user.reload.addresses
       render json: {
         status: 'ok',
-        data: render_to_string(file: 'addresses/index')
+        data: render_to_string('addresses/index')
       }
     else
       render json: {
         status: 'error',
-        data: render_to_string(file: 'addresses/new')
+        data: render_to_string('addresses/new')
       }
     end
   end
@@ -54,7 +54,7 @@ class AddressesController < ApplicationController
     @addresses = current_user.addresses
     render json: {
       status: 'ok',
-      data: render_to_string(file: 'addresses/index')
+      data: render_to_string('addresses/index')
     }
   end
 
@@ -65,7 +65,7 @@ class AddressesController < ApplicationController
     @addresses = current_user.reload.addresses
     render json: {
       status: 'ok',
-      data: render_to_string(file: 'addresses/index')
+      data: render_to_string('addresses/index')
     }
   end
 
