9d1572c8f5c2f769dd870bd91256f0ea0f94566a
zya <395549131@qq.com>
2023-01-05 23:55:00 +0800

    admin

--- project/app/controllers/admin/base_controller.rb
+++ project/app/controllers/admin/base_controller.rb
@@ -2,4 +2,11 @@ class Admin::BaseController < ActionController::Base
 
   layout 'admin/layouts/admin'
 
+  private
+  def auth_admin
+    unless logged_in? and current_user.is_admin?
+      flash[:notice] = "请以管理员身份登录"
+      redirect_to new_session_path
+    end
+  end
 end
