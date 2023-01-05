21ea6669a9cde3e6f9ac8b36bdab74e37f193db9
zya <395549131@qq.com>
2023-01-05 18:05:39 +0800

    admin product image

--- project/db/migrate/20230105100320_create_product_images.rb
+++ project/db/migrate/20230105100320_create_product_images.rb
@@ -1,7 +1,9 @@
 class CreateProductImages < ActiveRecord::Migration[7.0]
   def change
     create_table :product_images do |t|
-
+      t.belongs_to :product
+      t.integer :weight, default: 0
+      t.attachment :image
       t.timestamps
     end
   end
