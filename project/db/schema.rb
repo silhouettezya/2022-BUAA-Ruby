a3dcaea9e18ca50601d97f50f6ef1c6fef99cadb
20373384 <395549131@qq.com>
2023-01-05 16:17:50 +0000

    web

--- project/db/schema.rb
+++ project/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_05_143134) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_05_155159) do
   create_table "addresses", force: :cascade do |t|
     t.integer "user_id"
     t.string "address_type"
@@ -93,6 +93,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_05_143134) do
     t.integer "access_count_to_reset_password_page", default: 0
     t.string "uuid"
     t.integer "default_address_id"
+    t.boolean "is_admin", default: false
     t.index ["activation_token"], name: "index_users_on_activation_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
